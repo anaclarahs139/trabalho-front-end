f1c06cce6c12d911c3549eee7251368c44c177af
anaclarahs139 <anaclarahs139@gmail.com>
2025-06-02 16:23:25 -0300

    filtro

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ async function pegarEventos() {
         dados.eventos.forEach(evento => {
             const novaLinha = document.createElement('option');
 
-            novaLinha.textContent = `${evento.pedido} - ${evento.nome}   ${evento.data}`;
+            novaLinha.textContent = `${evento.nome}`;
             select.appendChild(novaLinha);
 
         });
@@ -35,26 +35,56 @@ function pegarReembolso() {
     var conteudo = document.querySelector('.conteudo-reembolsos-em-analise');
 
 
+
     let cod = `
                             <div class="conteudo-reembolso-em-analise">
                             <div class="titulo-reembolso-pendente">
-                                <h3 class="nome-evento-do-reembolso">Festival da Musica</h3>
+                                <h3 class="nome-evento-do-reembolso">${valorIngresso}</h3>
                                 <p class="reembolso-pendente">Em analise</p>
                             </div><!--titulo-status-reembolso-->
 
                             <div class="preco-botao-reembolso">
                                 <p class="preco-reembolso">R$ 120</p>
-                                <button class="cancelar-reembolso">cancelar Reembolso</button>
+                                <button class="cancelar-reembolso" onclick="cancelarReembolso()">cancelar Reembolso</button>
                             </div><!--preco-botao-reembolso-->
 
                             <div class="motivo-reembolso">
-                                <p class="motivo"><b>Motivo:</b> Emergência de trabalho</p>
-                                <p class="observacao"><b>Observação:</b> Nenhuma observação</p>
+                                <p class="motivo"><b>Motivo:</b> ${valorMotivo}</p>
+                                <p class="observacao"><b>Observação:</b> ${obs}</p>
                             </div><!--motivo-reembolso-->
                         </div><!--conteudo-rembolso-em-analise-->`;
 
     conteudo.innerHTML += cod;
-    conteudo.appendChild(cod);
+    
+};
 
+
+function cancelarReembolso() {
+    const botao = event.target; // o botão clicado
+    const reembolso = botao.closest('.conteudo-reembolso-em-analise');
+    if (reembolso) {
+        reembolso.remove(); // remove todo o bloco daquele reembolso
+    }
+}
+
+function abrirFiltro(){
+    document.querySelector('.overlay').style.display = "block";
+    document.querySelector('.div-filtro').style.display = "flex";
+}
+
+function aplicarFiltro(){
+    document.querySelector('.div-filtro').style.display = "none";
+    document.querySelector('.overlay').style.display = "none";
+}
+
+function preco(){
+    var preco = document.querySelector('#filtro-preco').value;
+    var div = document.querySelector('.valor').innerHTML = "";
+
+    var div = document.querySelector('.valor');
+    
+
+    
+    div.innerHTML += `R$: ${preco}`;
 
 }
